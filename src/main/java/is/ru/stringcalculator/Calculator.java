9c3b4ab7d0fb03312ec07f7645a0d84887c80ab4
jodiserla <jodis.erla@gmail.com>
2017-10-15 21:24:23 +0000

    Refactor: beautified code for emtpy string returns 0 and return sum of 2 numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,16 +3,19 @@ package is.ru.stringcalculator;
 public class Calculator {
     
     public static int add(String text) {
-        if(text == "") {
+        if(text.equals("")) {
             return 0;
         }
         else {
             if(text.contains(",")) {
                 String numbers[] = text.split(",");
-                return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+                return toInt(numbers[0]) + toInt(numbers[1]);
             }
             return 1;
         }
+    }
+    private static int toInt(String number) {
+        return Integer.parseInt(number);
     }
 }
 
